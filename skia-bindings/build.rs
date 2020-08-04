7535295e192461fc37634d2094b8eea9aac1fa23
Stewart Connor <stewart@canva.com>
2020-08-04 15:07:00 +1000

    build

--- skia-bindings/build.rs
+++ skia-bindings/build.rs
@@ -34,7 +34,8 @@ mod env {
 
     /// Force to build Skia, even if there is a binary available.
     pub fn force_skia_build() -> bool {
-        cargo::env_var("FORCE_SKIA_BUILD").is_some()
+        true
+        //cargo::env_var("FORCE_SKIA_BUILD").is_some()
     }
 
     /// A boolean specifying whether to build Skia's dependencies or not. If not, the system's
