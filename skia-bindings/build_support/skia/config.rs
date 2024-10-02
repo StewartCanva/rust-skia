ab69c9c97441141d3d9bd9ccee0ffceeaa5cdbe6
Dan Bishop <danbishop@canva.com>
2024-10-02 14:08:48 +1000

    remove freetype_woff2 new line

--- skia-bindings/build_support/skia/config.rs
+++ skia-bindings/build_support/skia/config.rs
@@ -138,7 +138,7 @@ impl FinalBuildConfiguration {
                 .arg("skia_use_freetype_woff2", yes())
                 .arg("skia_use_xps", no())
                 .arg("skia_use_dng_sdk", yes_if(features.dng))
-                .arg("skia_use_freetype_woff2", yes_if(features.freetype_woff2))
+                // .arg("skia_use_freetype_woff2", yes_if(features.freetype_woff2))
                 .arg("cc", quote(&build.cc))
                 .arg("cxx", quote(&build.cxx));
 
